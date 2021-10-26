bb4d4e98809243e11185d9587cb4d589fc5b35eb
jewel-saha-82 <rdtipl@hotmail.com>
2021-10-26 19:36:57 +0530

    Kafka producer consumer unit tests.

---
+++ src/main/java/com/kafka/testing/kafkatest/User.java
@@ -0,0 +1,42 @@
+package com.kafka.testing.kafkatest;
+
+public class User {
+
+    private String uuid;
+    private String firstName;
+    private String lastName;
+
+    public User() {
+    }
+
+    public User(String uuid, String firstName, String lastName) {
+        this.uuid = uuid;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
---
+++ src/test/java/com/kafka/testing/kafkatest/KafkaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.testing.kafkatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
